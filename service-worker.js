2f5df071ec3908e9b249a19794cf130861c74d64
ramy393-cmd <ramy393@gmail.com>
2026-04-20 14:03:23 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,18 @@
-const CACHE="words-tool-final-v2";
+const CACHE="words-tool-final-v3";
 
 self.addEventListener("install",e=>{
 self.skipWaiting();
-e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))
-);
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
 });
 
 self.addEventListener("activate",e=>{
-e.waitUntil(
-caches.keys().then(keys=>
-Promise.all(keys.map(k=>k!==CACHE && caches.delete(k)))
-)
-);
+e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k)))));
 self.clients.claim();
 });
 
 self.addEventListener("fetch",e=>{
 if(e.request.url.includes("script.google.com")){
-e.respondWith(
-fetch(e.request).catch(()=>new Response(JSON.stringify({data:[]}),{
-headers:{"Content-Type":"application/json"}
-}))
-);
+e.respondWith(fetch(e.request).catch(()=>new Response(JSON.stringify({data:[]}),{headers:{"Content-Type":"application/json"}})));
 return;
 }
 e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
